01aefb9a67b5d9616f42bda7fc95eae5beb1961f
Steve Braaksma <stevebraaksma@hotmail.com>
2021-12-06 08:56:57 -0600

    update resume file path

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 // import Steve_Braaksma_resume from "../Steve_Braaksma_resume.pdf"
-import resume from "../../public/static/resume.pdf"
+import resume from "/public/static/resume.pdf"
 
 const Header = ({ siteTitle }) => (
   <header
