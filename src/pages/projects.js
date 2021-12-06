f2a9623b7e4838ee0e471266b4de9f6b49cc08f3
Steve Braaksma <stevebraaksma@hotmail.com>
2021-12-06 11:19:23 -0600

    add additional icons

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -44,6 +44,13 @@ const Projects = () => (
           <a href="https://github.com/stevebraaksma/thresher/" target="_blank" rel="noreferrer"><GitHubIcon/></a>&nbsp;&nbsp;
           <a href="https://thresherapp.herokuapp.com/" target="_blank" rel="noreferrer"><OpenInNewIcon/></a><br />
           <h6 className="stack-card"> HTML / CSS / JavaScript / Node / Auth0</h6>
+          <img src={'/nodejs.svg'} alt="NodeJS" />
+          <img src={'/mongodb.svg'} alt="MongoDB" />
+          <img className="express-icon" src={'/expressjs.svg'} alt="Express" />
+          <img className="auth0-icon" src={'/auth0.svg'} alt="auth0" />
+          <img src={'/javascript.svg'} alt="JavaScript" />
+          <img src={'/css3.svg'} alt="css3" />
+          <img src={'/html5.svg'} alt="html5" />
         </Card>
       </Grid>
 
@@ -54,6 +61,11 @@ const Projects = () => (
           <a href="https://github.com/stevebraaksma/Triplog/" target="_blank" rel="noreferrer"><GitHubIcon/></a>&nbsp;&nbsp;
           <a href="https://triplogapp.herokuapp.com/" target="_blank" rel="noreferrer"><OpenInNewIcon/></a><br />
           <h6 className="stack-card"> HTML / CSS / JavaScript / React / Node</h6>
+          <img src={'/python.svg'} alt="Python" />
+          <img src={'/django.svg'} alt="Django" />
+          <img src={'/javascript.svg'} alt="JavaScript" />
+          <img src={'/css3.svg'} alt="css3" />
+          <img src={'/html5.svg'} alt="html5" />
         </Card>
       </Grid>
 
@@ -65,6 +77,10 @@ const Projects = () => (
           <a href="https://github.com/stevebraaksma/festivalRambler/" target="_blank" rel="noreferrer"><GitHubIcon/></a>&nbsp;&nbsp;
           <a href="https://festivalrambler.netlify.app/" target="_blank" rel="noreferrer"><OpenInNewIcon/></a><br />
           <h6 className="stack-card"> HTML / CSS / JavaScript / React / Node</h6>
+          <img src={'/jquery.svg'} alt="jquery" />
+          <img src={'/javascript.svg'} alt="JavaScript" />
+          <img src={'/css3.svg'} alt="css3" />
+          <img src={'/html5.svg'} alt="html5" />
         </Card>
       </Grid>
 
