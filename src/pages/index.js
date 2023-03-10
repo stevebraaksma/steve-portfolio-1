96855e820549245ef8eaa4accce5d759b31e6680
Steve Braaksma <stevebraaksma@hotmail.com>
2023-03-10 08:22:30 -0600

    update intro paragraphs

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,11 +21,14 @@ const IndexPage = () => (
     <h2>Software Engineer / Full Stack Developer</h2>
     
 
-    <p>
+    {/* <p>
     Transitioning from supply chain to developer, I am eager to use a new set of tools
     to solve business challenges.
     Open to frontend, backend and full stack opportunities.
-    </p>  
+    </p>   */}
+    <p>I’m a web developer with InterVarsity Christian Fellowship/USA.</p>
+    <p>Currently I work mostly with Drupal on the frontend, with JavaScript and Bootstrap. I am also enjoying learning headless Drupal with Drupal’s JSON API module, as well as module building.</p>
+    <p>I’m also interested in side projects, get in touch if you have a website or app idea you’d like to discuss!</p>
     <br />
     <Link className="resume-button nav" to="/projects">
       SEE MY PROJECTS
